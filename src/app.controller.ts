6aee3df15e51c56fb20646922d5e0ddaf541a42d
yukiharayama-para <b-o-c.bbcn.zone07@outlook.jp>
2021-05-23 23:18:08 +0900

    second commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get , Redirect ,Post } from '@nestjs/common';
 import { AppService, PublicItem } from './app.service';
 
 @Controller()
